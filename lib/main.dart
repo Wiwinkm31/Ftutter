137d906d9bf585dbdfca3db0f685612d90b8f692
Wiwin <wiwinkm31@gmail.com>
2022-03-09 00:10:22 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      Center(
+        child: Text(
+          'Selamat Berkunjung Di Akun WIWINK',
+          textDirection: TextDirection.ltr,
+        ),
+      )
+  );
+}
